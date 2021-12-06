4f88c6d9715c5a9da11e2831af9c44e410ce5a75
Alex Boyd <40293068+alex-d-boyd@users.noreply.github.com>
2021-12-06 23:21:37 +0900

    Add day 6 solution

--- Day_06/day_06.py
+++ Day_06/day_06.py
@@ -6,6 +6,7 @@
 
 import argparse
 
+from collections import Counter
 from pathlib import Path
 
 def parse_args():
@@ -14,7 +15,27 @@ def parse_args():
     args = parser.parse_args()
     return args
 
+def brute_force(start, days):
+    school = start[:]
+    for _ in range(days):
+        for i in range(len(school)):
+            if school[i] == 0:
+                school[i] = 6
+                school.append(8)
+            else:
+                school[i] -= 1
+    return len(school)
 
+def count_fish(start, days, debug=False):
+    counter = Counter(start)
+    add_in_two = 0
+    add_in_one = 0
+    for i in range(days):
+        temp = add_in_one
+        add_in_one = add_in_two
+        add_in_two = counter[i % 7]
+        counter[i % 7] += temp
+    return counter.total() + add_in_one + add_in_two
 
 if __name__ == '__main__':
     args = parse_args()
@@ -23,4 +44,12 @@ if __name__ == '__main__':
     else:
         in_file = Path('input.txt')
     puzzle_input = in_file.read_text(encoding='utf-8')
+    puzzle_input = list(map(int,puzzle_input.splitlines()[0].split(',')))
+
+    num = count_fish(puzzle_input, 80)
+    print(f'Part 1: Number of lanternfish after 80 days: {num}')
+
+    num = count_fish(puzzle_input, 256)
+    print(f'Part 1: Number of lanternfish after 256 days: {num}')
+
 
