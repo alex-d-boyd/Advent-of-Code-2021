4488c13b65df326d0218cfdd2a8ae62f8c30d4e0
Alex Boyd <40293068+alex-d-boyd@users.noreply.github.com>
2021-12-08 14:46:47 +0900

    Add day 7 solution

--- Day_07/day_07.py
+++ Day_07/day_07.py
@@ -14,6 +14,8 @@ def parse_args():
     args = parser.parse_args()
     return args
 
+def fuel(a, b):
+    return (d := abs(a - b)) * (d + 1) // 2
 
 
 if __name__ == '__main__':
@@ -22,5 +24,21 @@ if __name__ == '__main__':
         in_file = Path('test.txt')
     else:
         in_file = Path('input.txt')
-    puzzle_input = in_file.read_text(encoding='utf-8')
+    puzzle_input = in_file.read_text(encoding='utf-8').splitlines()
 
+    positions = list(map(int, puzzle_input[0].split(',')))
+
+    min_fuel_part_1 = float('inf')
+    min_fuel_part_2 = float('inf')
+    for alignment in range(min(positions), max(positions)+1):
+        fuel_part_1 = sum(abs(alignment - position) for position in positions)
+        if fuel_part_1 < min_fuel_part_1:
+            min_fuel_part_1 = fuel_part_1
+            min_pos_part_1 = alignment
+        fuel_part_2 = sum(fuel(alignment, position) for position in positions)
+        if fuel_part_2 < min_fuel_part_2:
+            min_fuel_part_2 = fuel_part_2
+            min_pos_part_2 = alignment
+
+    print(f'Part 1: Minimum fuel required is {min_fuel_part_1} at position {min_pos_part_1}')
+    print(f'Part 1: Minimum fuel required is {min_fuel_part_2} at position {min_pos_part_2}')
