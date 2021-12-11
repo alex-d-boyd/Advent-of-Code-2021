9114790ba96a7ffae8bbcb6138f87bfcf2ff657c
Alex Boyd <40293068+alex-d-boyd@users.noreply.github.com>
2021-12-11 15:27:14 +0900

    Add day 9 solution

--- Day_09/day_09.py
+++ Day_09/day_09.py
@@ -6,7 +6,10 @@
 
 import argparse
 
+from collections import deque
 from pathlib import Path
+
+DELTAS = ((-1, 0), (0, 1), (1, 0), (0, -1))
 
 def parse_args():
     parser = argparse.ArgumentParser(description='AoC 2021 Day 01')
@@ -14,7 +17,36 @@ def parse_args():
     args = parser.parse_args()
     return args
 
+def scan_low_points(height_map):
+    rows, cols = len(height_map), len(height_map[0])
+    lows = []
+    for r in range(rows):
+        for c in range(cols):
+            if all(height_map[r][c] < height_map[r2][c2]
+                   for dr, dc in DELTAS
+                   if 0 <= (r2 := r+dr) < rows and 0 <= (c2 := c+dc) < cols):
+                lows.append(((r, c), height_map[r][c]))
+    return lows
 
+def find_basins(height_map, lows):
+    def adjacents(point):
+        r, c = point
+        return [(r2, c2) for dr, dc in DELTAS
+                if 0 <= (r2 := r+dr) < rows and 0 <= (c2 := c+dc) < cols]
+    basins = []
+    rows, cols = len(height_map), len(height_map[0])
+    for low in lows:
+        q = deque()
+        q.append(low)
+        basin = set()
+        while q:
+            point = q.popleft()
+            r, c = point
+            if height_map[r][c] != 9 and point not in basin:
+                basin.add(point)
+                q.extend(adjacents(point))
+        basins.append(basin)
+    return basins
 
 if __name__ == '__main__':
     args = parse_args()
@@ -24,3 +56,14 @@ if __name__ == '__main__':
         in_file = Path('input.txt')
     puzzle_input = in_file.read_text(encoding='utf-8')
 
+    height_map = [list(map(int,list(line)))
+                  for line in puzzle_input.splitlines()]
+
+    lows = scan_low_points(height_map)
+    risk = sum(i + 1 for p, i in lows)
+    print(f'Part 1: Total risk is {risk}')
+
+    basins = sorted(find_basins(height_map, [p for p, i in lows])
+                    , key=len, reverse=True)
+    prod = len(basins[0]) * len(basins[1]) * len(basins[2])
+    print(f'Part 2: Product of top three basins is {prod}')
